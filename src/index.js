d4ae50221c02a364e508ad05cc869c09d6082130
xxx465465 <xxx46546500@163.com>
2017-11-26 13:42:39 +0800

    'tan'

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const reducers=(state,action)=>{
+	switch(action.type){
+		case "ADD_TODO":
+			var newState = Object.assign({},state);
+			newState.list.push(action.payload);
+			return newState;
+	} 
+		
+}
+const store = createStore(reducers,{list:[]})
+function renderPage(){
+	ReactDOM.render(<App store={store} />, document.getElementById('root'));
+
+}
+renderPage();
+store.subscribe( renderPage)
 registerServiceWorker();
