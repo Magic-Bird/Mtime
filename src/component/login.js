565e51d6ab73cd8e5b2963214e1e785d47464251
xxx465465 <xxx46546500@163.com>
2017-11-27 10:16:45 +0800

    'tan'

--- src/component/login.js
+++ src/component/login.js
@@ -1,5 +1,12 @@
 import React,{Component} from 'react';
 import axios from 'axios'
+import '../login.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
 
 
 export default class Login extends Component {
@@ -10,7 +17,11 @@ export default class Login extends Component {
 		return(
 			<div>
 				<div>
-				登陆
+					<input type="text" placeholder="请输入账号" />
+					<input type="password" placeholder="请输入密码" />
+					<button>登陆</button>
+					<button><Link to="/regist">注册</Link></button>
+
 				</div>
 			</div>
 		)
