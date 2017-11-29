a8be91f4f6b1873fa9d767051c7a2930cb2f4152
xxx465465 <xxx46546500@163.com>
2017-11-29 13:26:46 +0800

    'tan'

--- src/component/discovery.js
+++ src/component/discovery.js
@@ -1,11 +1,60 @@
 import React,{Component} from 'react';
 import axios from 'axios'
+import '../discovery.css'
 
 
 export default class Discovery extends Component {
+  	constructor(){
+  	  super();
+  	  this.state={
+  	    message:[],
+	
+  	  }
+  	}
+	
+  	componentDidMount(){
+ 	
+ 	
+	
+  	  var that = this;
+  	  axios.get(`Service/callback.mi/News/NewsList.api?t=2017112912443373702&pageIndex=1`)
+  	  .then(function(response){
+  	    console.log(response.data)
+  	    that.setState({
+  	      message:response.data.newsList
+	
+  	    })
+  	    
+	
+  	  })
+	
+  	  
+  	}	
 	render(){
+		var news=this.state.message.map((item,index)=>{
+			return(
+				<div key={item.id} className="list">
+					{
+						item.type===1?
+						<div className='type1'>
+							<div>{item.title}</div>
+							<img src={item.images[0].url1} />
+							<img src={item.images[1].url1} />
+							<img src={item.images[2].url1} />
+						</div>
+						:<div className='typeo'>
+							<img src={item.image} />
+							<div>{item.title}</div>
+						</div>
+					}
+					
+				</div>
+			)
+		})
 		return(
-			<div>发现</div>
+			<div>
+				{news}
+			</div>
 		)
 	}
 	
